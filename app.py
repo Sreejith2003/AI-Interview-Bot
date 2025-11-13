0164da28d23b5ac774d9c623a13f7e46948bba43
Sreejith <sreejith20off@gmail.com>
2025-11-13 18:59:11 +0530

    index.html is changed

--- app.py
+++ app.py
@@ -59,4 +59,4 @@ def static_files(filename):
     return send_from_directory("static", filename)
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8000, debug=True)
+    app.run(host="0.0.0.0", port=8000, debug=False)
